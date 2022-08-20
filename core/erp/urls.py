fa3dd0552c989debdb41afbfb0915cc7a82f0d59
Jose Diaz <jdiazrojas82@gmail.com>
2022-08-20 05:28:58 -0400

    Primera list view

---
+++ core/erp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = 'erp_app'
+
+urlpatterns = [
+    path('', HomePage.as_view(), name='home'),
+
+    #Category
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
+]
--- core/erp/views.py
+++ core/erp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import *
 
 # Create your views here.
+class HomePage(generic.TemplateView):
+    template_name = 'index.html'
+
+class CategoryListView(generic.ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Categorías'
+        return context
